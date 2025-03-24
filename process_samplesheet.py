6210b6cc58fb511107a27271a46616a6b568fc62
SummerYYY96 <summeryang96@gmail.com>
2025-03-24 13:43:11 -0400

    added argument parser and removed redundant lines

--- process_samplesheet.py
+++ process_samplesheet.py
@@ -1,31 +1,34 @@
 import os
 import glob
 import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f','--filepath', type=str,
+                   help='file path to all all the 65+ patients with Tumor and Normal Sample IDs')
+parser.add_argument("-o", "--output", help = "Output Path")
+
+args = parser.parse_args()
+csv_file_path = args.filepath
+output_dir = args.output
 
 def find_matching_file(directory, id):
-    # Use glob to search for files containing the ID
-    print(directory)
-    print(id)
-    pattern = os.path.join(directory, f"*{id}*.bam")
+    # Search for files containing the ID
+    pattern = os.path.join(directory, f"*{id}*.dd.ra.rc.bam")
     matching_files = glob.glob(pattern)
-    print(matching_files)
     
     # Check if any files were found
     if matching_files:
-        return matching_files[0]  # Return the first match (assuming there's a unique match)
+        return matching_files[0]  # Return the first match
     else:
         return None
 
 # Load the CSV file
-csv_file_path = "/gpfs/data/molecpathlab/development/chip_strelka_runs/data/AACR_953_age_final_65_plus.csv"
 df = pd.read_csv(csv_file_path)
-# Randomly select five rows
-sampled_df = df.sample(n=5, random_state=42)  # random_state is set for reproducibility
-# Create the "Sample" column
+sampled_df = df
 sampled_df["Sample"] = sampled_df["Tumor"] + "_" + sampled_df["Normal"]
 
-# Base directory path
-base_path = "output/alignments/deduplicated"
+base_path = "output/alignments/recalibrated"
 
 # Find matching BAM and BAI files
 sampled_df["TumorBam"] = sampled_df.apply(lambda row: find_matching_file(os.path.join(row['Run'],base_path), row['Tumor']), axis=1)
@@ -34,11 +37,10 @@ sampled_df["TumorBai"] = sampled_df["TumorBam"].apply(lambda x: x + ".bai" if x 
 sampled_df["NormalBam"] = sampled_df.apply(lambda row: find_matching_file(os.path.join(row['Run'],base_path), row['Normal']), axis=1)
 sampled_df["NormalBai"] = sampled_df["NormalBam"].apply(lambda x: x + ".bai" if x else None)
 
-# Reorder columns for output
+# Get samplesheet columns
 output_df = sampled_df[["Sample", "Tumor", "Normal", "TumorBam", "TumorBai", "NormalBam", "NormalBai"]]
 
-# Save the result to a TSV file
-output_file_path = "/gpfs/data/molecpathlab/development/chip_strelka_runs/data/sample.pairs.tsv"
+output_file_path = os.path.join(output_dir,"sample.pairs.tsv")
 output_df.to_csv(output_file_path, sep="\t", index=False)
-sample_df_path = "/gpfs/data/molecpathlab/development/chip_strelka_runs/data/sample_df.csv"
+sample_df_path = os.path.join(output_dir,"sample_df_all.csv")
 sampled_df.to_csv(sample_df_path, index=False)
